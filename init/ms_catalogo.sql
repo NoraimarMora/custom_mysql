b64076a969edf708a87877c40f68d468e0a16cdd
Noraimar Mora <noraimar.mora@gmail.com>
2019-10-14 00:52:43 -0400

    Actualizacion de ms_catalogo y ms_marcas

--- init/ms_catalogo.sql
+++ init/ms_catalogo.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-10-2019 a las 06:33:17
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.10
+
 -- Create ms_catalogo database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS `ms_catalogo`;
 -- Grant all privilidges on ms_catalogo to root
@@ -8,8 +17,20 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Estructura de las tablas
+-- Base de datos: `ms_catalogo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `adonis_schema`
 --
 
 CREATE TABLE `ms_catalogo`.`adonis_schema` (
@@ -19,76 +40,122 @@ CREATE TABLE `ms_catalogo`.`adonis_schema` (
   `migration_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `adonis_schema`
+--
+
 INSERT INTO `ms_catalogo`.`adonis_schema` (`id`, `name`, `batch`, `migration_time`) VALUES
-(1, '1560480112873_categorias_schema', 1, '2019-09-28 19:29:25'),
-(2, '1560480205281_caracteristicas_schema', 2, '2019-09-28 19:30:07'),
-(3, '1560480258902_valores_caracteristica_schema', 2, '2019-09-28 19:30:08'),
-(4, '1560480268043_tiendas_schema', 2, '2019-09-28 19:30:09'),
-(5, '1560480269217_productos_schema', 2, '2019-09-28 19:30:11'),
-(6, '1560717581982_categoria_producto_schema', 2, '2019-09-28 19:30:15'),
-(7, '1560717902378_producto_valor_caracteristica_schema', 2, '2019-09-28 19:30:17');
+(1, '1560480112873_categorias_schema', 1, '2019-10-14 04:30:14'),
+(2, '1560480205281_caracteristicas_schema', 1, '2019-10-14 04:30:14'),
+(3, '1560480258902_valores_caracteristica_schema', 1, '2019-10-14 04:30:15'),
+(4, '1560480268043_tiendas_schema', 1, '2019-10-14 04:30:17'),
+(5, '1560480269217_productos_schema', 1, '2019-10-14 04:30:18'),
+(6, '1560717581982_categoria_producto_schema', 1, '2019-10-14 04:30:21'),
+(7, '1560717902378_producto_valor_caracteristica_schema', 1, '2019-10-14 04:30:24');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `caracteristicas`
+--
 
 CREATE TABLE `ms_catalogo`.`caracteristicas` (
   `id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
 
 CREATE TABLE `ms_catalogo`.`categorias` (
   `id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `banner_url` varchar(255) DEFAULT NULL,
-  `padre` varchar(255) DEFAULT '0',
-  `posicion` int(11) DEFAULT NULL,
-  `activo` tinyint(1) DEFAULT NULL,
+  `parent` varchar(255) DEFAULT '0',
+  `position` int(11) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_producto`
+--
 
 CREATE TABLE `ms_catalogo`.`categoria_producto` (
   `id` int(10) UNSIGNED NOT NULL,
-  `categoria_id` int(10) UNSIGNED DEFAULT NULL,
-  `producto_id` int(10) UNSIGNED DEFAULT NULL,
+  `category_id` int(10) UNSIGNED DEFAULT NULL,
+  `product_id` int(10) UNSIGNED DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
 
 CREATE TABLE `ms_catalogo`.`productos` (
   `id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `referencia` varchar(255) DEFAULT NULL,
-  `precio` double DEFAULT NULL,
-  `descripcion` text,
+  `name` varchar(255) DEFAULT NULL,
+  `reference` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `description` text,
   `imagen_url` varchar(255) DEFAULT NULL,
-  `personalizable` tinyint(1) DEFAULT NULL,
-  `tienda_id` int(10) UNSIGNED DEFAULT NULL,
-  `activo` tinyint(1) DEFAULT NULL,
+  `customizable` tinyint(1) DEFAULT NULL,
+  `store_id` int(10) UNSIGNED DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto_valor_caracteristica`
+--
 
 CREATE TABLE `ms_catalogo`.`producto_valor_caracteristica` (
   `id` int(10) UNSIGNED NOT NULL,
-  `valor_caracteristica_id` int(10) UNSIGNED DEFAULT NULL,
-  `producto_id` int(10) UNSIGNED DEFAULT NULL,
-  `impacto` double DEFAULT '0',
+  `feature_value_id` int(10) UNSIGNED DEFAULT NULL,
+  `product_id` int(10) UNSIGNED DEFAULT NULL,
+  `impact` double DEFAULT '0',
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiendas`
+--
 
 CREATE TABLE `ms_catalogo`.`tiendas` (
   `id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `valores_caracteristicas`
+--
+
 CREATE TABLE `ms_catalogo`.`valores_caracteristicas` (
   `id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `caracteristica_id` int(10) UNSIGNED DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `feature_id` int(10) UNSIGNED DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -97,58 +164,103 @@ CREATE TABLE `ms_catalogo`.`valores_caracteristicas` (
 -- Índices para tablas volcadas
 --
 
+--
+-- Indices de la tabla `adonis_schema`
+--
 ALTER TABLE `ms_catalogo`.`adonis_schema`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `caracteristicas`
+--
 ALTER TABLE `ms_catalogo`.`caracteristicas`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `categorias`
+--
 ALTER TABLE `ms_catalogo`.`categorias`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `categoria_producto`
+--
 ALTER TABLE `ms_catalogo`.`categoria_producto`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `categoria_producto_categoria_id_foreign` (`categoria_id`),
-  ADD KEY `categoria_producto_producto_id_foreign` (`producto_id`);
+  ADD KEY `categoria_producto_category_id_foreign` (`category_id`),
+  ADD KEY `categoria_producto_product_id_foreign` (`product_id`);
 
+--
+-- Indices de la tabla `productos`
+--
 ALTER TABLE `ms_catalogo`.`productos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `productos_tienda_id_foreign` (`tienda_id`);
+  ADD KEY `productos_store_id_foreign` (`store_id`);
 
+--
+-- Indices de la tabla `producto_valor_caracteristica`
+--
 ALTER TABLE `ms_catalogo`.`producto_valor_caracteristica`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `producto_valor_caracteristica_valor_caracteristica_id_foreign` (`valor_caracteristica_id`),
-  ADD KEY `producto_valor_caracteristica_producto_id_foreign` (`producto_id`);
+  ADD KEY `producto_valor_caracteristica_feature_value_id_foreign` (`feature_value_id`),
+  ADD KEY `producto_valor_caracteristica_product_id_foreign` (`product_id`);
 
+--
+-- Indices de la tabla `tiendas`
+--
 ALTER TABLE `ms_catalogo`.`tiendas`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `valores_caracteristicas`
+--
 ALTER TABLE `ms_catalogo`.`valores_caracteristicas`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `valores_caracteristicas_caracteristica_id_foreign` (`caracteristica_id`);
+  ADD KEY `valores_caracteristicas_feature_id_foreign` (`feature_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
+--
+-- AUTO_INCREMENT de la tabla `adonis_schema`
+--
 ALTER TABLE `ms_catalogo`.`adonis_schema`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
+--
+-- AUTO_INCREMENT de la tabla `caracteristicas`
+--
 ALTER TABLE `ms_catalogo`.`caracteristicas`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
 ALTER TABLE `ms_catalogo`.`categorias`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `categoria_producto`
+--
 ALTER TABLE `ms_catalogo`.`categoria_producto`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
 ALTER TABLE `ms_catalogo`.`productos`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `producto_valor_caracteristica`
+--
 ALTER TABLE `ms_catalogo`.`producto_valor_caracteristica`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `valores_caracteristicas`
+--
 ALTER TABLE `ms_catalogo`.`valores_caracteristicas`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
@@ -156,17 +268,33 @@ ALTER TABLE `ms_catalogo`.`valores_caracteristicas`
 -- Restricciones para tablas volcadas
 --
 
+--
+-- Filtros para la tabla `categoria_producto`
+--
 ALTER TABLE `ms_catalogo`.`categoria_producto`
-  ADD CONSTRAINT `categoria_producto_categoria_id_foreign` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `categoria_producto_producto_id_foreign` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `categoria_producto_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categorias` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `categoria_producto_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE;
 
+--
+-- Filtros para la tabla `productos`
+--
 ALTER TABLE `ms_catalogo`.`productos`
-  ADD CONSTRAINT `productos_tienda_id_foreign` FOREIGN KEY (`tienda_id`) REFERENCES `tiendas` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `productos_store_id_foreign` FOREIGN KEY (`store_id`) REFERENCES `tiendas` (`id`) ON DELETE CASCADE;
 
+--
+-- Filtros para la tabla `producto_valor_caracteristica`
+--
 ALTER TABLE `ms_catalogo`.`producto_valor_caracteristica`
-  ADD CONSTRAINT `producto_valor_caracteristica_producto_id_foreign` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `producto_valor_caracteristica_valor_caracteristica_id_foreign` FOREIGN KEY (`valor_caracteristica_id`) REFERENCES `valores_caracteristicas` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `producto_valor_caracteristica_feature_value_id_foreign` FOREIGN KEY (`feature_value_id`) REFERENCES `valores_caracteristicas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `producto_valor_caracteristica_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE;
 
+--
+-- Filtros para la tabla `valores_caracteristicas`
+--
 ALTER TABLE `ms_catalogo`.`valores_caracteristicas`
-  ADD CONSTRAINT `valores_caracteristicas_caracteristica_id_foreign` FOREIGN KEY (`caracteristica_id`) REFERENCES `caracteristicas` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `valores_caracteristicas_feature_id_foreign` FOREIGN KEY (`feature_id`) REFERENCES `caracteristicas` (`id`) ON DELETE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- init/ms_marcas.sql
+++ init/ms_marcas.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-10-2019 a las 06:47:15
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.10
+
 -- Create ms_marcas database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS `ms_marcas`;
 -- Grant all privilidges on ms_marcas to root
@@ -8,36 +17,66 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Estructura de las tablas
+-- Base de datos: `ms_marcas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuentas_bancarias`
 --
 
 CREATE TABLE `ms_marcas`.`cuentas_bancarias` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `beneficiario` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tipo_dni` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
+  `beneficiary` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dni_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `dni` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `num_cuenta` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tipo_cuenta` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tienda_id` bigint(20) UNSIGNED NOT NULL
+  `account_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `account_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `store_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horarios`
+--
 
 CREATE TABLE `ms_marcas`.`horarios` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `dia` enum('1','2','3','4','5','6','7') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abierto` int(11) NOT NULL,
-  `cerrado` int(11) NOT NULL,
-  `tienda_id` bigint(20) UNSIGNED NOT NULL
+  `id` int(10) UNSIGNED NOT NULL,
+  `day` enum('1','2','3','4','5','6','7') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `open` int(11) NOT NULL,
+  `close` int(11) NOT NULL,
+  `store_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marcas`
+--
+
 CREATE TABLE `ms_marcas`.`marcas` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `logo_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `banner_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `activo` tinyint(1) NOT NULL
+  `active` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
 
 CREATE TABLE `ms_marcas`.`migrations` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -45,72 +84,125 @@ CREATE TABLE `ms_marcas`.`migrations` (
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
 INSERT INTO `ms_marcas`.`migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2019_05_14_013439_create_marcas_table', 1),
 (2, '2019_05_14_013538_create_tiendas_table', 1),
 (3, '2019_05_14_013608_create_cuentas_bancarias_table', 1),
 (4, '2019_05_14_013636_create_horarios_table', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiendas`
+--
+
 CREATE TABLE `ms_marcas`.`tiendas` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `latitud` double NOT NULL,
-  `longitud` double NOT NULL,
-  `marca_id` bigint(20) UNSIGNED NOT NULL,
-  `activo` tinyint(1) NOT NULL
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `brand_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
+--
+-- Indices de la tabla `cuentas_bancarias`
+--
 ALTER TABLE `ms_marcas`.`cuentas_bancarias`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `cuentas_bancarias_tienda_id_foreign` (`tienda_id`);
+  ADD KEY `cuentas_bancarias_store_id_foreign` (`store_id`);
 
+--
+-- Indices de la tabla `horarios`
+--
 ALTER TABLE `ms_marcas`.`horarios`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `horarios_tienda_id_foreign` (`tienda_id`);
+  ADD KEY `horarios_store_id_foreign` (`store_id`);
 
+--
+-- Indices de la tabla `marcas`
+--
 ALTER TABLE `ms_marcas`.`marcas`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `migrations`
+--
 ALTER TABLE `ms_marcas`.`migrations`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `tiendas`
+--
 ALTER TABLE `ms_marcas`.`tiendas`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `tiendas_marca_id_foreign` (`marca_id`);
+  ADD KEY `tiendas_brand_id_foreign` (`brand_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
+--
+-- AUTO_INCREMENT de la tabla `cuentas_bancarias`
+--
 ALTER TABLE `ms_marcas`.`cuentas_bancarias`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `horarios`
+--
 ALTER TABLE `ms_marcas`.`horarios`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `marcas`
+--
 ALTER TABLE `ms_marcas`.`marcas`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
 ALTER TABLE `ms_marcas`.`migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
+--
+-- AUTO_INCREMENT de la tabla `tiendas`
+--
 ALTER TABLE `ms_marcas`.`tiendas`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
 --
 
+--
+-- Filtros para la tabla `cuentas_bancarias`
+--
 ALTER TABLE `ms_marcas`.`cuentas_bancarias`
-  ADD CONSTRAINT `cuentas_bancarias_tienda_id_foreign` FOREIGN KEY (`tienda_id`) REFERENCES `tiendas` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `cuentas_bancarias_store_id_foreign` FOREIGN KEY (`store_id`) REFERENCES `tiendas` (`id`) ON DELETE CASCADE;
 
+--
+-- Filtros para la tabla `horarios`
+--
 ALTER TABLE `ms_marcas`.`horarios`
-  ADD CONSTRAINT `horarios_tienda_id_foreign` FOREIGN KEY (`tienda_id`) REFERENCES `tiendas` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `horarios_store_id_foreign` FOREIGN KEY (`store_id`) REFERENCES `tiendas` (`id`) ON DELETE CASCADE;
 
+--
+-- Filtros para la tabla `tiendas`
+--
 ALTER TABLE `ms_marcas`.`tiendas`
-  ADD CONSTRAINT `tiendas_marca_id_foreign` FOREIGN KEY (`marca_id`) REFERENCES `marcas` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `tiendas_brand_id_foreign` FOREIGN KEY (`brand_id`) REFERENCES `marcas` (`id`) ON DELETE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
